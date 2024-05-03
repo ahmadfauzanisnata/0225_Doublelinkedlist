13ca0b0562bc7571337e71725ce3bade972eb658
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-03 14:34:36 +0700

    "Implementasi operasi sisipan dengan benar dalam linked list berganda

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -12,24 +12,24 @@ struct Node
 Node *START = NULL;
 void addNode()
 {
-    void addNode() {
+     
     Node* newNode = new Node(); // step 1: create a new node
     cout << "\nEnter the roll number of the student: ";
     cin >> newNode->noMhs;     // Assign value to the data field of the new node
     cout << "\nEnter the name of the student: ";
     cin >> newNode->name;   // Assign value to the data field of the new node
-    }
+    
     // insert the new node in the list
-    if (START == NULL || nownode->noMhs <= START->noMHS)
+    if (START == NULL || newNode->noMhs <= START->noMhs)
     { // step 2 insert the new node at the beginning 
 
-    if (START != NULL && newNodde->noMhs == START->noMhs)
+    if (START != NULL && newNode->noMhs == START->noMhs)
     {
         cout << "\033[31mDuplicate roll numbers not allowed\033]0m" << endl;
         return;
     }
     // if the list is empty, make the new node the START
-    nowNode->next = START; // step 3: make the new node point to the new first node
+    newNode->next = START; // step 3: make the new node point to the new first node
     if (START != NULL)
     {
         START->prev = newNode; // step 4: make the first node point to the first node
@@ -49,7 +49,11 @@ void addNode()
             previous = current;        // step 1.d: move the previous to the current position
             current = current->next; // step 1.e: move the current to the next node
         }
+        newNode->next = current;  // step 4: make the next field of the new node point to current.
+        newNode->prev = previous; // step 5: make the previous field of the new node point to previous.
 
+       
+    
         
     }
 }
