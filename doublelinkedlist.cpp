6e5aa1b48303acd436b7dd70e0658238ad128357
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-03 13:54:23 +0700

    Mengatur pointer START ke node baru

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -35,6 +35,7 @@ void addNode()
         START->prev = newNode; // step 4: make the first node point to the first node
     }
     newNode->prev = NULL; // step 5: make the new node point to NULL
+    START = newNode;
 
     }
 }
